6f472492be991a9cc12633748c2822e11d687ff6
Alvinchi1 <alvinchibundo007>
2024-11-02 17:02:19 -0700

    alx-react

---
+++ task_0/src/index.js
@@ -0,0 +1,5 @@
+const $ = require( "jquery" );
+
+$('body').append('<p>Holberton Dashboard</P>');
+$('body').append('<p>Dashboard data for students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
